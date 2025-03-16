1db19e783118ef4c670ce117d122f01de2835333
youssif ahmed <joahmed002@gmail.com>
2025-03-16 13:52:50 +0200

    Enhance inventory fact transformer to handle missing date records and improve error handling

--- server/src/services/etl/transformers/inventoryFactTransformer.js
+++ server/src/services/etl/transformers/inventoryFactTransformer.js
@@ -1,24 +1,30 @@
 const prisma = require("../../../../prisma/dwh/client");
 
-async function inventoryFactTransformer(rawData) {
-  const date = new Date();
-  date.setDate(date.getDate() - 1);
+async function inventoryFactTransformer(rawData, date = null) {
+  try {
+    date = date || new Date(new Date().setDate(new Date().getDate() - 1));
 
-  const dateId = await prisma.DateDimension.findFirst({
-    where: {
-      fullDate: date,
-    },
-    select: { dateId: true },
-  });
-  const transformedData = rawData.map((record) => {
-    return {
-      businessId: record.businessId,
-      productId: record.productId,
-      dateId: dateId.dateId,
-      currentStock: record.currentStock,
-    };
-  });
-  return transformedData;
+    const dateRecord = await prisma.DateDimension.findFirst({
+      where: {
+        fullDate: date,
+      },
+      select: { dateId: true },
+    });
+    if (!dateRecord) {
+      throw new Error(`No DateDimension entry for ${date}`);
+    }
+    const transformedData = rawData.map((record) => {
+      return {
+        businessId: record.businessId,
+        productId: record.productId,
+        dateId: dateRecord.dateId,
+        currentStock: record.currentStock,
+      };
+    });
+    return transformedData;
+  } catch (error) {
+    console.error("Transformation failed:", error);
+  }
 }
 
 module.exports = inventoryFactTransformer;
