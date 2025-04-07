81d69ee123f07293ccd7b744a8aaf95497e0da26
AhmedGhazal112 <ahmedmedooo580@gmail.com>
2025-04-07 22:50:32 +0200

    Some fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,6 +20,7 @@ import QuarterlyRevenues from "./pages/dashboard/QuarterlyRevenues";
 import Transactions from "./pages/dashboard/Transactions";
 import ResetPassword from "./pages/Auth/ResetPassword";
 import CsvSubmissions from "./pages/TransactionsFeeding/CsvSubmissions";
+import Header from "./components/common/Header";
 
 function App() {
   return (
@@ -122,7 +123,8 @@ function App() {
           path="/csvsubmissions"
           element={
             <ProtectedRoute>
-              <div className="mt-24">
+              <Header></Header>
+              <div className="mt-4">
                 <CsvSubmissions />
               </div>
             </ProtectedRoute>
