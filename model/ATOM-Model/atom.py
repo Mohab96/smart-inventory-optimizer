d214ba0c183e38f87e13cc32a3cc5ed55d742f74
yousseflashin <youssef.mlashin.yml@gmail.com>
2025-04-29 00:32:05 +0300

    Remove unused ModelStorage class from atom.py

--- model/ATOM-Model/atom.py
+++ model/ATOM-Model/atom.py
@@ -19,14 +19,7 @@ import random
 
 
 # ========== Database Model ==========
-class ModelStorage(db.Model):
-    __tablename__ = 'model_storage'
-    id = db.Column(db.Integer, primary_key=True)
-    business_id = db.Column(db.String(255), nullable=False)
-    product_id = db.Column(db.Integer, nullable=False)
-    model_data = db.Column(db.LargeBinary)
-    scaler_data = db.Column(db.LargeBinary)
-    last_trained = db.Column(db.DateTime, default=datetime.utcnow)
+
 
 # ========== XGBoost Handler ==========
 class XGBoostPredictor:
