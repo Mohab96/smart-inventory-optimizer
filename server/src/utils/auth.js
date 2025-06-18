60d3f62060108d5402208f7ee27a36b4de31a34b
Youssif Ahmed <joahmed002@gmail.com>
2025-06-18 18:44:41 +0300

    Fix async token generation in authentication processes added  business details in the token

--- server/src/utils/auth.js
+++ server/src/utils/auth.js
@@ -1,16 +1,24 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const maindb = require("../../prisma/main/client");
 
 const hashPassword = async (password, saltRounds = 10) => {
   return await bcrypt.hash(password, saltRounds);
 };
 
-const generateToken = (user, expiration = "1h") => {
+const generateToken = async (user, expiration = "10h") => {
+  console.log(user);
+  const business = await maindb.business.findUnique({
+    where: { id: user.businessId },
+  });
+  console.log("Business: ", business);
   return jwt.sign(
     {
       userId: user.id,
       username: user.username,
       email: user.email,
+      name: user.name,
+      businessName: business.name,
       isAdmin: user.isAdmin,
       businessId: user.businessId,
     },
